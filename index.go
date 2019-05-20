55794a71ed430fcd10938322238a80baeded7556
senbiki <aki.sen.1209@gmail.com>
2019-05-20 22:04:04 +0900

    http server with go wo

--- index.go
+++ index.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"phpi/myreflect"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
+var err error
+var userRegex *regexp.Regexp
+var matchedValue [][]string
+
 func main() {
+	// URLパターンマッチング
+	userRegex, err = regexp.Compile("^/account/id/([0-9]+)/?$")
 	// 静的ファイル配信際に捜査するディレクトリ
 	var publicRoot string = "C:\\Users\\senbiki\\public"
 
@@ -50,7 +59,53 @@ func main() {
 		header = writer.Header()
 		header.Set("Content-RequestedURL", reqeustedURL)
 		header.Set("Content-Route", "/account/")
-		fmt.Fprint(writer, "Welcome to the page named /account/.")
+		// fmt.Fprint(writer, "Welcome to the page named /account/.")
+
+		pattern1, err := regexp.Compile("^/account/([0-9a-zA-Z_]+)/([0-9a-zA-Z_]+)/?$")
+		if err != nil {
+
+		}
+		if reqeustedURL == "/account/" {
+			var getParamter string = request.URL.RawQuery
+			var replacedParameter string = strings.Replace(getParamter, "imagePath=", "", -1)
+			res, _ := http.Get(replacedParameter)
+			binary, _ := ioutil.ReadAll(res.Body)
+			fmt.Fprint(writer, string(binary))
+			// var getParamter string = request.URL.RawQuery
+			// var reg *regexp.Regexp
+			// reg, _ = regexp.Compile("imagePath=(.*)$")
+			// matchedValue = reg.FindAllStringSubmatch(getParamter, -1)
+			// if len(matchedValue) > 0 {
+			// 	res, _ := http.Get(matchedValue[0][1])
+			// 	binary, _ := ioutil.ReadAll(res.Body)
+			// 	// fmt.Println(string(binary))
+			// 	header.Set("Content-Type", "image/jpeg")
+			// 	fmt.Fprint(writer, string(binary))
+			// }
+
+		} else if matchedValue = pattern1.FindAllStringSubmatch(reqeustedURL, -1); len(matchedValue) > 0 {
+			/** /acccount/Acategory/Bgenre のようなURLへのアクセス時 */
+			var mainCategory string
+			var subCategory string
+			mainCategory = matchedValue[0][1]
+			subCategory = matchedValue[0][2]
+			fmt.Fprintln(writer, "以下が閲覧中の情報です<br>")
+			fmt.Fprint(writer, mainCategory)
+			fmt.Fprint(writer, subCategory)
+		} else {
+			var userID int
+			var err error
+			matchedValue = userRegex.FindAllStringSubmatch(reqeustedURL, -1)
+			if len(matchedValue) > 0 {
+				userID, err = strconv.Atoi(matchedValue[0][1])
+				if err == nil {
+					fmt.Fprint(writer, "閲覧中のユーザーIDは"+strconv.Itoa(userID)+"です")
+				} else {
+					header.Set("Content-Type", "text/html;charset=UTF-8")
+					fmt.Println("ユーザIDの取得に失敗しました。")
+				}
+			}
+		}
 	})
 
 	// (2)会員登録ページを想定する
@@ -74,6 +129,7 @@ func main() {
 			// 指定したディレクトリをpublicルートとする
 			filesHandler = http.FileServer(http.Dir(publicRoot))
 			fixHandler = http.StripPrefix("/", filesHandler)
+
 			// ストリームに書き込み
 			fixHandler.ServeHTTP(writer, request)
 		}
